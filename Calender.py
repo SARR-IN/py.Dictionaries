c79a3ab6c97dedd16139b0b62fb89f8a271664b8
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-09-23 18:01:15 +0530

    Create Calender.py
    This code used to Easily find data of Leap year Calender.

---
+++ Calender.py
@@ -0,0 +1,17 @@
+# Python program to display calendar of
+# given month of the year
+
+# import module
+import calendar
+
+# //This code use for find only 1 month calender
+# yy = 2021
+# mm = 9
+#
+# # display the calendar
+# print(calendar.month(yy, mm))
+
+
+
+print ("The calendar of year 2018 is : ")
+print (calendar.calendar(2021, 2, 1, 6))
