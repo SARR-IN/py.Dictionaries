07bfd0dae925c8eafc00f1b79c57afed88ca62e3
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-10-05 19:27:08 +0530

    Create WhatsappMSG.py
    This Code uses for Whatsapp messages and sending messages using another social networking site.

---
+++ WhatsappMSG.py
@@ -0,0 +1,8 @@
+import pywhatkit as kit
+#pip install pywhatkit
+
+kit.sendwhatmsg("+919265667985","Hi, Kya Kar rahe ho", 18,15)
+
+# type Number is Recevier Number
+# Your typed message in middel in thee code
+# your preferred time is denoted by last of the code.
